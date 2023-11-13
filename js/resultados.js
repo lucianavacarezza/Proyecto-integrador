690f94eb2cc1030429afa9cdad45db05e9f93c4c
Mercedes Lapasset <mer@MacBook-Air-de-Mercedes.local>
2023-11-13 14:10:28 -0300

    Traje el objeto de peliculas de la base de datos

--- js/resultados.js
+++ js/resultados.js
@@ -1,5 +1,26 @@
-let apiKey = 'f5ffd30de9f03f8a95e76344c36e6cc3'
-let queryString= location.search
-let queryStringObj= new URLSearchParams(queryString)
-let valor= queryStringObj.get("buscador") //valor query que voy a usar
-fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}?query=${valor}`)
+let apiKey = 'f5ffd30de9f03f8a95e76344c36e6cc3';
+
+
+let queryString= location.search;
+let queryStringObj= new URLSearchParams(queryString);
+let valor= queryStringObj.get("buscador"); //valor query que voy a usar
+
+let link_busquedas= `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${valor}`;
+
+fetch(link_busquedas)
+
+.then(function (response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data)
+    
+
+
+})
+
+.catch(function(error){
+    console.log(error)
+
+
+})
