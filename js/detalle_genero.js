5622b9aad24fb8e89fc4eebd9dbdd331855bb6f6
Mercedes Lapasset <mer@MacBook-Air-de-Mercedes.local>
2023-11-15 09:23:07 -0300

    detalle genero

--- js/detalle_genero.js
+++ js/detalle_genero.js
@@ -1,4 +1,8 @@
 let apiKey = 'f5ffd30de9f03f8a95e76344c36e6cc3'
+//El listado de las películas o series pertenecientes al género que seleccionó. El end point para usar es "Discover".
+// El nombre del género. En caso de que no se encuentre el género o no exista, debe indicarse un mensaje de error.
+// Lista de películas o series con su foto y su nombre. Al clickear en cualquiera de las series/ películas debe redirigir al detalle (punto 4).
+'https://api.themoviedb.org/3/genre/movie/list?language=en' 
 
 fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`)
 
